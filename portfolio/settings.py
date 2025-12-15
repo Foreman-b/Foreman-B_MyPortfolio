bc07a54ee217aa20867e1a65efe75d1bcb8a9a67
Foreman-b <hamzat4life@gmail.com>
2025-12-15 10:57:25 +0100

    Add Django Portfolio Project for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
+ALLOWED_HOSTS = ["foremanbportfolio.onrender.com", ".onrender.com"]
 
 
 # Application definition
