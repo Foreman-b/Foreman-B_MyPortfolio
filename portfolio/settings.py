4d1c393d2f1f8cb9519ce418206b1415e04a6f24
Foreman-b <hamzat4life@gmail.com>
2025-12-15 10:33:15 +0100

    Add Django Portfolio Project for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v+i@jz0aczny7uqx^^1y*v8^#$*_n966aw(0*&fvh0v5^(r37j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
