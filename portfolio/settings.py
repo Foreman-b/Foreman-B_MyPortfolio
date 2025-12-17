e11f402e74c047f9e4e4cc0e9e399aae42585fe3
Foreman-b <hamzat4life@gmail.com>
2025-12-17 15:04:45 +0100

    Ad new updated settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",  "unsafe-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", ".onrender.com"]
+ALLOWED_HOSTS = ["localhost", "foremanbportfolio.onrender.com", '127.0.0.1']
 
 
 # Application definition
@@ -88,14 +90,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'portfolio_db',
-            'USER': 'DB_USER',
-            'PASSWORD': 'DB_PASSWORD',
-            'HOST': 'localhost',
-            'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
