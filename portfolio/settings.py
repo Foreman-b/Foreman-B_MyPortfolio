4bed234d9dfb1f3c9c763065e2ebb395ecc655bf
Foreman-b <hamzat4life@gmail.com>
2025-12-17 15:12:17 +0100

    Ad new updated settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY",  "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "foremanbportfolio.onrender.com", '127.0.0.1']
 
