5fa450d953bc2fad90e157e1ba6f46ef74b35107
Foreman-b <hamzat4life@gmail.com>
2025-12-17 13:41:49 +0100

    Add New Updated settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'portfolio_db',
+            'USER': 'DB_USER',
+            'PASSWORD': 'DB_PASSWORD',
+            'HOST': 'localhost',
+            'PORT': '5432',
     }
 }
 
