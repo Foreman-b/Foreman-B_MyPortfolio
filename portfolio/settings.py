1862477f8bcf4730adc7cdf41062d11a2004a9ea
Foreman-b <hamzat4life@gmail.com>
2026-06-22 14:58:40 -0500

    Set my debug to false in production

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY",  "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["foremanbportfolio.onrender.com", "https://foremanbportfolio.onrender.com", "localhost", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://foremanbportfolio.onrender.com"]
